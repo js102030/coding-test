13191e18d27f796c3333481827b1e17b572e788e
js102030 <121748946+js102030@users.noreply.github.com>
2024-01-19 10:12:11 +0900

    [level 1] Title: 시저 암호, Time: 2.80 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,20 +1,16 @@
 class Solution {
-    private char push(char c, int n) {
-        if (!Character.isAlphabetic(c)) {
-            return c;
-        }
-
-        int offset = Character.isUpperCase(c) ? 'A' : 'a';
-        int position = c - offset;
-        position = (position + n) % ('Z' - 'A' + 1);
-        return (char) (offset + position);
-    }
-
     public String solution(String s, int n) {
-        StringBuilder builder = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            builder.append(push(c, n));
+        StringBuilder result = new StringBuilder();
+        n = n % 26;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                ch = (char) ((ch - 'a' + n) % 26 + 'a');
+            } else if (Character.isUpperCase(ch)) {
+                ch = (char) ((ch - 'A' + n) % 26 + 'A');
+            }
+            result.append(ch);
         }
-        return builder.toString();
+        return result.toString();
     }
 }
