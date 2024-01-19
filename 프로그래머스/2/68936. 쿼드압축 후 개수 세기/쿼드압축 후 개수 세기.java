9ee2da6797759c848c057596cee9ea2005de2c63
js102030 <121748946+js102030@users.noreply.github.com>
2024-01-19 20:40:57 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 4.85 ms, Memory: 91.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -0,0 +1,40 @@
+class Solution {
+    private final class Count {
+        private final int zero;
+        private final int one;
+
+        private Count(int zero, int one) {
+            this.zero = zero;
+            this.one = one;
+        }
+    
+        public Count add(Count other) {
+            return new Count(zero + other.zero, one + other.one);
+        }
+
+    }
+
+    private Count count(int offsetX, int offsetY, int size, int[][] arr) {
+        int h = size / 2;
+        for (int y = offsetY; y < offsetY + size; y++) {
+            for (int x = offsetX; x < offsetX + size; x++) {
+                if (arr[y][x] != arr[offsetY][offsetX]) {
+                    return count(offsetX, offsetY, h, arr)
+                            .add(count(offsetX + h, offsetY, h, arr))
+                            .add(count(offsetX, offsetY + h, h, arr))
+                            .add(count(offsetX + h, offsetY + h, h, arr));
+                }
+            }
+        }
+
+        if (arr[offsetY][offsetX] == 1) {
+            return new Count(0, 1);
+        }
+        return new Count(1, 0);
+    }
+
+    public int[] solution(int[][] arr) {
+        Count count = count(0, 0, arr.length, arr);
+        return new int[]{count.zero, count.one};
+    }
+}
