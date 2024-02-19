50b7ba7d02d9f94954dded6ddf3a59a91a3ac93d
js102030 <121748946+js102030@users.noreply.github.com>
2024-02-19 21:10:13 +0900

    [level 1] Title: 체육복, Time: 1.34 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class Solution {
+   public static int solution(int n, int[] lost, int[] reserve) {
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+        Set<Integer> owns = Arrays.stream(lost)
+                .boxed()
+                .collect(Collectors.toSet());
+        owns.retainAll(Arrays.stream(reserve)
+                .boxed()
+                .collect(Collectors.toSet()));
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int l : lost) q.add(l);
+
+        int get = 0;
+        for (int r : reserve) {
+            if (owns.contains(r)) {
+                continue;
+            }
+
+            while (!q.isEmpty() && (q.peek() < r - 1 || owns.contains(q.peek()))) {
+                q.poll();
+            }
+            if (q.isEmpty()) break;
+
+            if (q.peek() <= r + 1) {
+                q.poll();
+                get++;
+            }
+        }
+
+        return n - lost.length + owns.size() + get;
+    }
+}
