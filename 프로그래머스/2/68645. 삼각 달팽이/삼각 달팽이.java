4b35bd097c4d74597cde64437071a537b8f9969a
js102030 <121748946+js102030@users.noreply.github.com>
2024-02-20 09:29:32 +0900

    [level 2] Title: 삼각 달팽이, Time: 16.90 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,7 +1,6 @@
 class Solution {
-    private static final int[] dx = {0, 1, -1};
+private static final int[] dx = {0, 1, -1};
     private static final int[] dy = {1, 0, -1};
-
 
     public int[] solution(int n) {
         int[][] triangle = new int[n][n];
@@ -18,7 +17,11 @@ class Solution {
                 d = (d + 1) % 3;
                 nx = x + dx[d];
                 ny = y + dy[d];
-                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) break;
+
+                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0){
+                    break;
+                }
+
             }
             x = nx;
             y = ny;
