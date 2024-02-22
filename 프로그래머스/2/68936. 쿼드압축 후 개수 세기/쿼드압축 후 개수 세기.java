1d651741b6892132c639182f04f5798e4cb67fd7
js102030 <121748946+js102030@users.noreply.github.com>
2024-02-22 20:45:22 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 8.24 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,23 +1,13 @@
 class Solution {
-    private final class Count {
-        private final int zero;
-        private final int one;
-
-        private Count(int zero, int one) {
-            this.zero = zero;
-            this.one = one;
-        }
-    
-        public Count add(Count other) {
-            return new Count(zero + other.zero, one + other.one);
-        }
-
+    public int[] solution(int[][] arr) {
+        Count count = count(0, 0, arr.length, arr);
+        return new int[] {count.zero, count.one};
     }
 
     private Count count(int offsetX, int offsetY, int size, int[][] arr) {
         int h = size / 2;
-        for (int y = offsetY; y < offsetY + size; y++) {
-            for (int x = offsetX; x < offsetX + size; x++) {
+        for (int x = offsetX; x < offsetX + size; x++) {
+            for (int y = offsetY; y < offsetY + size; y++) {
                 if (arr[y][x] != arr[offsetY][offsetX]) {
                     return count(offsetX, offsetY, h, arr)
                             .add(count(offsetX + h, offsetY, h, arr))
@@ -33,8 +23,17 @@ class Solution {
         return new Count(1, 0);
     }
 
-    public int[] solution(int[][] arr) {
-        Count count = count(0, 0, arr.length, arr);
-        return new int[]{count.zero, count.one};
+    private static class Count {
+        public final int zero;
+        public final int one;
+
+        public Count(int zero, int one) {
+            this.zero = zero;
+            this.one = one;
+        }
+
+        public Count add(Count other) {
+            return new Count(zero + other.zero, one + other.one);
+        }
     }
 }
