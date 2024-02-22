a2cfabede0e3d201f7ab5671056b85a0e6f7a986
js102030 <121748946+js102030@users.noreply.github.com>
2024-02-22 17:48:33 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.99 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,21 +1,20 @@
 class Solution {
-    public static int[] solution(String s) {
-int changeCount = 0;
-        int deletedZeroCount = 0;
-
-        String changedString = s;
-
-        do {
-            if (changedString.contains("0")) {
-                String replace = changedString.replace("0", "");
-                deletedZeroCount += changedString.length() - replace.length();
-                changedString = Integer.toString(replace.length(), 2);
-            } else {
-                changedString = Integer.toString(changedString.length(), 2);
+    public int[] solution(String s) {
+        int countDeletedZero = 0;
+        int changeCount = 0;
+        while (!s.equals("1")) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : s.toCharArray()) {
+                if (c == '1') {
+                    sb.append(c);
+                } else {
+                    countDeletedZero++;
+                }
             }
-            ++changeCount;
-
-        } while (!changedString.equals("1"));
-        return new int[]{changeCount, deletedZeroCount};
+            int length = sb.length();
+            s = Integer.toString(length, 2);
+            changeCount++;
+        }
+        return new int[]{changeCount, countDeletedZero};
     }
 }
